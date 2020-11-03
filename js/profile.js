8ed9dcec774ee245af33e573559fc5ead509e912
oscarCorb <hello@oscarcorb.com>
2020-11-03 21:33:14 +0100

    ADD: Add new song form functionality

--- js/profile.js
+++ js/profile.js
@@ -1,82 +1,24 @@
 
-
-
-// const addNewSong = () => {
-//     const addSongBtn = document.querySelector('.add-song-btn');
-//     const addSongText = document.querySelector('.add-song-text');
-
-//     addSongBtn.addEventListener('click', function () {
-//         // aquí tendré que ejecutar el form para abrirlo
-//         // y quitar el console.log
-//         console.log('click on button');
-//     });
-
-//     addSongText.addEventListener('click', function () {
-//         // aquí tendrá que ir la ejecución de abir formulario
-//         console.log('click on text');
-//         body.style.opacity = 0.5;
-//         // main.style.visibility = 'visible';
-//     });
-// }
-
-// addNewSong();
-
-
-
-
-
-
-
-// const displayNewSongForm = () => {
-
-//     const addSongBtn = document.querySelector('.add-song-btn');
-//     const addSongText = document.querySelector('.add-song-text');
-//     const modalContainer = document.querySelector('.modal-container');
-
-//     addSongBtn.addEventListener('click', () => {
-//         modalContainer.style.display = 'initial';
-//         addSongBtn.style.transform = 'rotate(45deg)';
-//     });
-
-//     addSongText.addEventListener('click', () => {
-//         modalContainer.style.display = 'initial';
-//         addSongBtn.style.transform = 'rotate(45deg)';
-//     });
-// }
-
-// displayNewSongForm();
-
-// const closeNewSongForm = () => {
-
-//     const closeBtnX = document.querySelector('.close-x');
-//     const modalContainer = document.querySelector('.modal-container');
-
-//     closeBtnX.addEventListener('click', () => {
-//         modalContainer.style.display = 'none';
-//     });
-// }
-
-// closeNewSongForm();
-
-
-
-
+// rotate button + to x when open form
 let toggleFormStatus = false;
 
 const toggleForm = () => {
     const modalContainer = document.querySelector('.modal-container');
     const addSongBtn = document.querySelector('.add-song-btn');
+    // const btnCloseX = document.querySelector('.btn-close-x');
 
     if (toggleFormStatus === false) {
 
         modalContainer.style.visibility = 'visible';
         addSongBtn.style.transform = 'rotate(45deg)';
+        // btnCloseX.style.backgroundColor = 'red';
         toggleFormStatus = true;
 
     } else if (toggleFormStatus === true) {
-        
+
         modalContainer.style.visibility = 'hidden';
         addSongBtn.style.transform = 'rotate(0deg)';
+        // btnCloseX.style.backgroundColor = '#38C48B';
         toggleFormStatus = false;
     }
 }
